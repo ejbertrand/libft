b6d20a80030cb795f34eeaba8a156110d439fe86
ejbertrand <eddas_email@yahoo.com>
2021-06-24 22:51:25 -0300

    Updating header and adding itoa method

---
+++ libft/ft_itoa.c
@@ -0,0 +1,51 @@
+#include "libft.h"
+
+static	int	isnegative(int n)
+{
+    if (n < 0)
+        return (1);
+    return (0);
+}
+
+static	int	num_order(unsigned int n)
+{
+    int    c;
+
+    c = 0;
+    if (n == 0)
+        return (1);
+    while (n >= 1)
+    {
+        n = n / 10;
+        c++;
+    }
+    return (c);
+}
+
+char    *ft_itoa(int n)
+{
+    unsigned int    order;
+    unsigned int    negative;
+    char            *new_st;
+    unsigned int    nbr;
+
+    negative = isnegative(n);
+    if (negative)
+        nbr = -n;
+    else
+        nbr = n;
+    order = num_order(nbr);
+    new_st = (char *)ft_calloc(negative + order + 1, sizeof(char));
+    if (!new_st)
+        return (NULL);
+    if (negative)
+		new_st[0] = '-';
+	new_st[negative + order] = '\0';
+    while (order > 0)
+    {
+        new_st[negative + order - 1] = (nbr % 10) + '0';
+        nbr = nbr / 10;
+        order--;
+    }
+    return (new_st);
+}
--- test.c
+++ test.c
@@ -6,12 +6,7 @@
 
 int main(void)
 {	
-	char * * tab = ft_split("tripouille , 42 , huhuhuhu, ffffff, chancho chanchia, fi ", ',');
-	printf("%s\n", tab[0]);
-	printf("%s\n", tab[1]);
-	printf("%s\n", tab[2]);
-	printf("%s\n", tab[3]);
-	printf("%s\n", tab[4]);
-	printf("%s\n", tab[5]);
+	char * s = ft_itoa(INT_MIN);
+	printf("%s\n", s);
     return (0);
 }
