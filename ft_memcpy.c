a25d219472b9e04d5dbf82c3df24e859eea7c9f4
ejbertrand <eddas_email@yahoo.com>
2021-06-29 23:55:09 -0300

    Add changes to memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -7,6 +7,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	aux_dest = dest;
 	aux_src = (void *)src;
+	if (dest == NULL || src == NULL)
+		return (NULL);
 	while (n-- > 0)
 	{
 		*aux_dest++ = *aux_src++;
