22cc4b2e3d1dcae885ee61de68629bb1d829a8b7
Eddas Josue bertrand martinez <ejosue-b@c1r1p4.42sp.org.br>
2021-06-30 00:02:28 -0300

    Adding memcpy change, to correct its behavior when receiving NULL pointers

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -7,7 +7,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	aux_dest = dest;
 	aux_src = (void *)src;
-	if (dest == NULL || src == NULL)
+	if (dest == NULL && src == NULL)
 		return (NULL);
 	while (n-- > 0)
 	{
