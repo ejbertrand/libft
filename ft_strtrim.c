5ee839688d9aba4b7c9cadd3759749e0ad4f79ef
ejbertrand <eddas_email@yahoo.com>
2021-06-29 23:35:16 -0300

    Correcting ft_strtrim!

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -7,10 +7,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char			*trimmed;
 
 	start = 0;
-	while (ft_strchr(set, s1[start]))
-		start++;
 	len = ft_strlen(s1);
-	if (start > len)
+	while ((start < len) && ft_strchr(set, s1[start]))
+		start++;
+	if (start == len)
 		return (ft_calloc(1, sizeof(char)));
 	while (len > 0 && ft_strchr(set, s1[len]))
 		len--;
