7497468d89954c9241452e2bdd687973f47d9383
ejbertrand <eddas_email@yahoo.com>
2021-06-30 00:07:46 -0300

    Adding correction to memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -9,6 +9,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	aux_dest = dest;
 	aux_src = (void *)src;
 	index = 0;
+	if (!dest && !src)
+		return (NULL);
 	if (dest < src)
 	{
 		while (index < n)
@@ -18,9 +20,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		}
 	}
 	else
-	{
 		while (n-- > 0)
 			aux_dest[n] = aux_src[n];
-	}
 	return (dest);
 }
