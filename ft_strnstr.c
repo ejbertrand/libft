319728712285d915c99e3dbde77feb75c3fd844c
ejbertrand <eddas_email@yahoo.com>
2021-06-29 21:14:35 -0300

    Correcting strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -16,16 +16,23 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const char	*aux_little;
 	const char	*aux_big;
+	char *end;
 
-	aux_big = big;
-	aux_little = little;
 	if (!*little)
 		return ((char *) big);
 	if (len == 0)
 		return (NULL);
-	while (*big != '\0' && len > 0)
+	aux_big = big;
+	aux_little = little;
+	if (len < ft_strlen(big))
+		end = (char *)&big[len - 1];
+	else
+		end = (char *)&big[ft_strlen(big) - 1];
+	//while (*big != '\0' && len > 0)
+	while (*big != '\0' && big <= end)
 	{
-		while ((len-- > 0) && (*big++ == *little++))
+		//while ((len-- > 0) && (*big++ == *little++))
+		while ((big <= end) && (*big++ == *little++))
 		{
 			if (*little == '\0')
 				return ((char *) aux_big);
